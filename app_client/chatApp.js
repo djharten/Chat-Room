69781a33c8954fa1cd5b0c4a512019c9048a2e1c
djharten <djharten@millersville.edu>
2019-05-06 21:56:19 -0400

    final

--- app_client/chatApp.js
+++ app_client/chatApp.js
@@ -181,8 +181,9 @@ app.controller('homeController', ['authentication', function homeController(auth
 
     var socket = io.connect('http://18.208.205.232');
     var msg = $("#message");
-    if(isLoggedIn)
+    if(isLoggedIn()) {
         var user = vm.currentUser().name;
+    }
     var sendMsg = $("#send_message");
     var chatRoom = $("#chatroom");
 
